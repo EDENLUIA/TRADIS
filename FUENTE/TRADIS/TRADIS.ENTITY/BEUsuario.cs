3ad5ceea6cfb8b5dc8c874a8b99914ad5ba49d85
Eden Ancieta - Home <edenluis_elae@hotmail.com>
2019-03-19 22:46:42 -0500

    Creando Solucion y Capa Entidad

---
+++ FUENTE/TRADIS/TRADIS.ENTITY/BEUsuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TRADIS.ENTITY
+{
+    public class BEUsuario
+    {
+        public int UsuarioId { get; set; }
+        public string Login { get; set; }
+        public string Nombres { get; set; }
+        public string ApellidoPaterno { get; set; }
+    }
+}
