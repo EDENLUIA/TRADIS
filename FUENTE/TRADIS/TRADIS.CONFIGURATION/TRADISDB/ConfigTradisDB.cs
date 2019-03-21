b7f81bf37a62cdd21cf8094951aa20aedb497c2a
Eden Ancieta - Home <edenluis_elae@hotmail.com>
2019-03-20 22:11:52 -0500

    Implementando Capa Configuration

---
+++ FUENTE/TRADIS/TRADIS.CONFIGURATION/TRADISDB/ConfigTradisDB.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace TRADIS.CONFIGURATION
+{
+   public class ConfigTradisDB
+    {
+        private static BDConfiguration mSigCom;
+        private static Mutex mMutex = new Mutex();
+        private static ConfigTradisDB mInstance;
+
+        public ConfigTradisDB(string aplicacion)
+        {
+            try
+            {
+                mSigCom = new BDConfiguration();
+                mSigCom = DatoProduccion(aplicacion);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private BDConfiguration DatoProduccion(string aplicacion)
+        {
+
+            mSigCom = new BDConfiguration
+            {
+                BaseDatos = "TRADISDB",
+                Usuario = "sa",
+                Password = "fisuncp",
+                Servidor = "localhost",
+                Provider = "SQL"
+            };
+            return mSigCom;
+        }
+
+        public static ConfigTradisDB GetInstance(string aplicacion)
+        {
+            mMutex.WaitOne();
+            if ((mInstance == null))
+            {
+                mInstance = new ConfigTradisDB(aplicacion);
+            }
+            mMutex.ReleaseMutex();
+            return mInstance;
+        }
+
+        public BDConfiguration Parametros
+        {
+            get
+            {
+                return mSigCom;
+            }
+        }
+    }
+}
